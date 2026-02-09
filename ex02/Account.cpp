940f5566eace9b92536928b2cd0491511fb42147
Seully Trieste <strieste@c1r9s4.42lausanne.ch>
2026-02-09 09:52:00 +0100

    Finish

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: strieste <strieste@student.42.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 11:36:26 by strieste          #+#    #+#             */
-/*   Updated: 2026/02/08 12:46:48 by strieste         ###   ########.fr       */
+/*   Updated: 2026/02/09 09:39:59 by strieste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,11 @@ int	Account::_totalNbWithdrawals = 0;
 
 Account::Account( int initial_deposit ): _accountIndex(_nbAccounts), _amount(initial_deposit)
 {
-	this->_accountIndex = _nbAccounts;
 	_nbAccounts++;
-	this->_amount = initial_deposit;
 	_totalAmount += _amount;
 	this->_nbDeposits = 0;
-	this->_totalNbWithdrawals = 0;
 	Account::_displayTimestamp();
-	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created";
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created";
 	std::cout << std::endl;
 	return ;
 }
@@ -38,6 +35,10 @@ Account::~Account(void)
 	Account::_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed";
 	std::cout << std::endl;
+	this->_amount = 0;
+	this->_accountIndex = 0;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
 	return ;
 }
 
@@ -62,33 +63,47 @@ int	Account::getNbWithdrawals( void )
 void	Account::displayAccountsInfos( void )
 {
 	Account::_displayTimestamp();
-	std::cout << "account:" << &getNbAccounts << ";total:" << &getTotalAmount;
-	std::cout << ";deposits:" << &getNbDeposits << ";withdrawals:" << &getNbWithdrawals;
+	std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount;
+	std::cout << ";deposits:" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals;
 	std::cout << std::endl;
 	return ;
 }
 
 void	Account::makeDeposit( int deposit )
 {
-	this->_amount += deposit;
-	this->_totalAmount += deposit;
-	this->_totalNbDeposits++;
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount;
+	if (deposit > 0)
+	{
+		this->_amount += deposit;
+		this->_nbDeposits++;
+		_totalNbDeposits++;
+		_totalAmount += deposit;
+	}
+	std::cout << ";deposit:" << deposit << ";amount:" << this->_amount;
+	std::cout << ";nb_deposits:" << this->_nbDeposits;
+	std::cout << std::endl;
 	return ;
 }
 bool	Account::makeWithdrawal( int withdrawal )
 {
-		if (withdrawal >= this->_amount)
-		{
-			this->_amount -= withdrawal;
-			this->_totalAmount -= withdrawal;
-			this->_totalNbWithdrawals++;
-			return (true);
-		}
-		Account::_displayTimestamp();
+	Account::_displayTimestamp();
+	if (withdrawal > this->_amount)
+	{
 		std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount;
 		std::cout << ";withdrawal:refused";
 		std::cout << std::endl;
 		return (false);
+	}
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount;
+	this->_amount -= withdrawal;
+	this->_nbWithdrawals++;
+	_totalAmount -= withdrawal;
+	_totalNbWithdrawals++;
+	std::cout << ";withdrawal:" << withdrawal << ";amount:" << this->_amount;
+	std::cout << ";nb_withdrawals:" << this->_nbWithdrawals;
+	std::cout << std::endl;
+	return (true);
 }
 int		Account::checkAmount( void ) const
 {
@@ -99,18 +114,18 @@ void	Account::displayStatus( void ) const
 {
 	Account::_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";amount:";
-	std::cout << this->_amount << ";deposits:" << this->_nbDeposits;
-	std::cout << ";withdrawals:" << this->_nbWithdrawals;
+	std::cout << this->_amount << ";deposits:" << _nbDeposits;
+	std::cout << ";withdrawals:" << _nbWithdrawals;
 	std::cout << std::endl;
 	return ;
 }
 
 void	Account::_displayTimestamp( void )
 {
-	std::time_t	timestamp = std::time(nullptr);
+	std::time_t	timestamp = std::time(NULL);
 	std::tm		datetime = *localtime(&timestamp);
 
-	char	output[50];
+	char	output[13];
 	strftime(output, 50, "[%Y%m%d_%I%M%S]", &datetime);
 	std::cout << output << " ";
 	return ;
